19b9cc7d60fcdae323b3e88eb02f315f4317b9ba
David Cid <david.cid@gmail.com>
2020-02-13 22:31:25 +0100

    added stars rating to the cards

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,12 +10,21 @@ class Products extends React.Component {
         <div className="card" key={product.title}>
           <p className="type">{product.type}</p>
           <img src={imgPath} alt={product.title} />
+          <div className="stars">{this.showStars(product.rating)}</div>
           <h3>{product.title}</h3>
           <p>{product.description}</p>
           <h4>{product.price}€</h4>
         </div>
       );
     });
+  };
+
+  showStars = rating => {
+    let stars = "";
+    for (let i = 0; i < rating; i++) {
+      stars += "⭐️";
+    }
+    return stars;
   };
 
   render() {
